0e186f98d2392c0cad91c748e6a81fb88d3318e6
rutvikpipariya <125720851+rutvikpipariya@users.noreply.github.com>
2023-03-13 13:41:05 +0530

    Create introduction of c.c

---
+++ assignment/modul 1/introduction of c.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main ()
+{
+    printf("name : rutvik pipaliya \n");
+    printf("birth date :27/09/2003 \n");
+    printf("age : 20\n");
+    printf("address : madhav park sheri no 4, oppo.real prime ,bapa sitaram chowk ,pin - 360004 \n");
+}
