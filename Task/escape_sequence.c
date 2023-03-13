1274f463b0b06879da9602b8b44b7d85e1e9caad
rutvikpipariya <125720851+rutvikpipariya@users.noreply.github.com>
2023-03-13 12:52:32 +0530

    Create escape_sequence.c

---
+++ Task/escape_sequence.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+    //use of \n
+    printf("village:chandali\n");
+    //use of \t
+    printf("\ttakuko:lodhika\n");
+    //use of \r
+    printf("district\r:rajkot\n");
+    //use of \b
+    printf("pincode\b :360035\n");
+    //use of \f
+    printf("what is programme\f-it is a set of instruction \n  ");
+    //use of \v
+    printf("what is programming \v-to creat a programme");
+    return 0;
+}
