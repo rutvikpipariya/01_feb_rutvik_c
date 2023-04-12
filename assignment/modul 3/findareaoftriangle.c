4ee826454ae6d7b6739d779cbe2e3ee728286bed
Rutvik Pipaliya <125720851+rutvikpipariya@users.noreply.github.com>
2023-04-12 19:29:51 +0530

    Create findareaoftriangle.c

---
+++ assignment/modul 3/findareaoftriangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    float hight;
+    float base;
+    float area;
+    printf("Enter Hight of Triangle : ");
+    scanf("%f",&hight);
+    printf("\nEnter Base of Triangle : ");
+    scanf("%f",&base);
+    area=hight*base / 2;
+    printf("\nArea of Triangle is : %f",area);
+
+}
