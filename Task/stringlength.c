cf62d01f25474771bcb79174c30f8e3de50d5ee1
Rutvik Pipaliya <125720851+rutvikpipariya@users.noreply.github.com>
2023-04-15 09:17:32 +0530

    Create stringlength.c

---
+++ Task/stringlength.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char name[20];
+    int length;
+    printf("Enter Your Name :");
+    scanf("%s", &name);
+    length = strlen(name);
+    printf("Length of %s is %d", name, length);
+}
