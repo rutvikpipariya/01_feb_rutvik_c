4f70a5bc4885cb5fc876d30840a3c63fa57cea03
Rutvik Pipaliya <125720851+rutvikpipariya@users.noreply.github.com>
2023-04-15 10:52:30 +0530

    Create summationnumber.c

---
+++ assignment/modul 3/summationnumber.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter any number : ");
+    scanf("%d",&num);
+    int a,b = 0;
+    for(int i = 1; num%10>=1; i++){
+        a = num%10;
+        b = b + a;
+        num = num/10;
+    }
+    printf("The sum of given number is %d",b);
+    return 0;
+}
