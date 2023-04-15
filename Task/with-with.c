6fbc78ea3ec8b52747a5ffdd8028e64d1b41fc3b
Rutvik Pipaliya <125720851+rutvikpipariya@users.noreply.github.com>
2023-04-15 09:29:33 +0530

    Create with-with.c

---
+++ Task/with-with.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int sub(int a, int b)
+{
+    return a-b;
+}
+int main()
+{
+    int x,y;
+    printf("Enter the value of X :");
+    scanf("%d",&x);
+    printf("Enter the value of y :");
+    scanf("%d",&y);
+    printf("%d",sub(x,y));
+    return 0;
+}
