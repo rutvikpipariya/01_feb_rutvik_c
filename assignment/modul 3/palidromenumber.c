efaffacdc03d65f41a822f9d7d9d9b82062e01b0
Rutvik Pipaliya <125720851+rutvikpipariya@users.noreply.github.com>
2023-04-15 10:35:35 +0530

    Create palidromenumber.c

---
+++ assignment/modul 3/palidromenumber.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char n[20];
+    int length;
+    printf("Enter any number : ");
+    scanf("%s",&n);    
+    length = strlen(n);    
+    int x = length - 1;    
+    int z = 0;
+            for(int i = 0; i < length/2; i++){
+                if(n[i] == n[x]){
+                    z++;
+                }
+                x--;
+            }    
+    if(z == length/2){
+        printf("\nNumber is palindrome");
+    }   
+    else{
+        printf("\nNumber is not palindrome");
+    }    
+    return 0;
+}
