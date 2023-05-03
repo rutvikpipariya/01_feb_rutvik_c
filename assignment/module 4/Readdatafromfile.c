e8ee35a021fdc8886d472cfeaebf8115e42a46ec
Rutvik Pipaliya <125720851+rutvikpipariya@users.noreply.github.com>
2023-05-03 17:12:31 +0530

    Create Readdatafromfile.c

---
+++ assignment/module 4/Readdatafromfile.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    FILE *fptr;
+    char filedata[20];
+    char nm[20];
+    printf("Enter Your Name :");
+    scanf("%s",&nm);
+    fptr = fopen("D:/internal storage/new.txt", "r");
+    fprintf(fptr,"%s",nm);
+    fclose(fptr);    
+    return 0;
+}
